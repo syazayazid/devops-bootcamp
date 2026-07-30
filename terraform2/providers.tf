2fab15a0adb99c0b8267fb73446dfba2e2c0594d
Syaza <syazayazid@gmail.com>
2026-07-30 21:57:20 +0800

    remote backend

--- terraform2/providers.tf
+++ terraform2/providers.tf
@@ -6,6 +6,13 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket       = "bootcamp-2026-syaza"
+    key          = "terraform2/terraform.tfstate"
+    region       = "ap-southeast-1"
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
