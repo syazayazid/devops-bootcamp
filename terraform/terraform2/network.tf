156e15dd0e91ec39d1356f9f772fe042c4fb058b
Syaza <syazayazid@gmail.com>
2026-09-13 14:53:26 +0800

    add private subnet

--- terraform/terraform2/network.tf
+++ terraform/terraform2/network.tf
@@ -8,10 +8,20 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.20.1.0/24"
-  availability_zone       = "ap-southeast-1a"
+  availability_zone       = var.az
   map_public_ip_on_launch = true
   tags = {
     Name = "tf-subnet-public"
+  }
+}
+
+resource "aws_subnet" "my_private_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.20.2.0/24"
+  availability_zone       = var.az
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "tf-subnet-private"
   }
 }
 
@@ -35,4 +45,16 @@ resource "aws_route" "my_route" {
 resource "aws_route_table_association" "my_link" {
   subnet_id      = aws_subnet.my_subnet.id
   route_table_id = aws_route_table.my_route_table.id
+}
+
+resource "aws_route_table" "my_private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "tf-rt-private"
+  }
+}
+
+resource "aws_route_table_association" "my_private_link" {
+  subnet_id      = aws_subnet.my_private_subnet.id
+  route_table_id = aws_route_table.my_private_route_table.id
 }
