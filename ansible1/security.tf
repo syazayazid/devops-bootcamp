eb54bd7df6104094a26fc4eaeb68770274aa7b67
Syaza <syazayazid@gmail.com>
2026-09-14 10:27:39 +0800

    ansible 1

---
+++ ansible1/security.tf
@@ -0,0 +1,34 @@
+data "http" "myip" {
+  url = "https://ifconfig.me/ip"
+}
+
+module "my_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 6.0"
+
+  name            = "tf-vpc-sg"
+  use_name_prefix = false
+  vpc_id          = module.my_vpc.vpc_id
+
+  ingress_rules = {
+
+    ssh = {
+      cidr_ipv4   = "${chomp(data.http.myip.response_body)}/32"
+      ip_protocol = "tcp"
+      from_port   = 22
+      to_port     = 22
+    }
+    http = {
+      cidr_ipv4   = "0.0.0.0/0"
+      ip_protocol = "tcp"
+      from_port   = 80
+      to_port     = 80
+    }
+  }
+
+  egress_rules = {
+    all = { cidr_ipv4 = "0.0.0.0/0", ip_protocol = "-1" }
+  }
+
+  tags = { Name = "tf-vpc-sg" }
+}
